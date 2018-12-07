3521490fd4cd74ef25c70159ad074b2c8e1cfce8
realv <realv@LAPTOP-Q46B21SH>
2018-12-07 11:46:19 +0100

    Bootstrap into a maven project for spring dependancys..

---
+++ SpringIOC/SpringIOC/src/main/ioc/Mobile.java
@@ -0,0 +1,18 @@
+package com.tsu.ioc;
+
+import org.omg.CORBA.portable.ApplicationException;
+import org.springframework.beans.factory.xml.XmlReaderContext;
+import org.springframework.context.ApplicationContext;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+	//	Converting to spring
+	//	Sim sim = new Airtel();
+	//	sim.calling();
+	//	sim.data();
+
+		ApplicationContext myAppC =ApplicationContext new XmlReaderContext();
+	}
+
+}
